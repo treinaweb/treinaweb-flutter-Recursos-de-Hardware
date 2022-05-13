ff73bafb47718c5a9d1f6578d16204dc734af069
Ariel Sardinha <ariel.sardinha@treinaweb.com.br>
2022-05-13 12:14:52 -0300

    giroscópio

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:hardware/camera/view.dart';
 import 'package:hardware/face_touch_id/view.dart';
 import 'package:hardware/geolocalizacao/view.dart';
-import 'package:hardware/sensores/view.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({Key? key}) : super(key: key);
@@ -35,14 +34,6 @@ class CustomDrawer extends StatelessWidget {
               ));
             },
             child: const Text('Touch Face ID'),
-          ),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).pushReplacement(MaterialPageRoute(
-                builder: (ctx) => const SensoresView(),
-              ));
-            },
-            child: const Text('Sensores'),
           )
         ],
       ),
